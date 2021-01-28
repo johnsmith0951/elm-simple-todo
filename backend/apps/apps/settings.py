16520821b3dd2209453cbc9ef4555f8ecbccdbf6
kai <skybird1233@gmail.com>
2021-01-29 01:44:49 +0900

    fixed todo list

--- backend/apps/apps/settings.py
+++ backend/apps/apps/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'apps.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
+        "default": env.db_url("DATABASE_URL", engine="django.db.backends.postgresql")
         }
 
 # Password validation
